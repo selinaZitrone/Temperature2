fe6ba59c178da9c470d04a8395c330bf56a1ea2f
SelinaZitrone <selina.baldauf@gmx.de>
2020-10-08 11:47:06 +0200

    Add tests

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(Temperature2)
+
+test_check("Temperature2")
---
+++ tests/testthat/test-printMean.R
@@ -0,0 +1,3 @@
+test_that("Printer function works", {
+  expect_equal(printMean(c(1,2,3)), expected = "The mean value is 2!")
+})
